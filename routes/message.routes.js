8bbcf70b0f90b53d9c6c4fb1287ff4bcf2e98806
alberto-sclocchi <scloks75tv@gmail.com>
2025-04-14 22:53:54 -0400

    re-format phone number before saving message

--- routes/message.routes.js
+++ routes/message.routes.js
@@ -6,14 +6,23 @@ const Message = require("../models/Message.model");
 router.post("/", (req, res, next) => {
   console.log(req.body);
 
-  const { firstName, lastName, email, phoneNumber, message } = req.body;
+  const { firstName, lastName, email, phoneNumber, message, countryCode} = req.body;
 
-  if(firstName === "" || lastName === ""  || email  === "" || phoneNumber  === "" || message === "" ){
+  if(firstName === "" || lastName === ""  || email  === "" || phoneNumber  === "" || message === "" || countryCode === ""){
     res.json({message: "Please fill in the required info before proceeding."});
     return;
   }
 
-  Message.create(req.body)
+  //re-format phone number
+  const updateMessage = {
+    firstName,
+    lastName,
+    email,
+    phoneNumber: `+${countryCode} ${phoneNumber}`,
+    message
+  }
+
+  Message.create(updateMessage)
   .then((message) => {
     res.json({message: "Thank you for reaching out to us! We have received your message and will get back to you as soon as possible.", success: true});
   })
